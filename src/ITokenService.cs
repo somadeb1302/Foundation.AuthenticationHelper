8e83137ff3906f01dc9da8fc7fa4237c4ef82f93
Debaditya De <somadeb1302.de@outlook.com>
2024-12-29 22:12:19 +0530

    first commit in auth helper

---
+++ src/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Foundation.AuthenticationHelper;
+public interface ITokenService
+{
+    string GenerateAccessToken(string userId, string role, string ipAddress, string userAgent);
+    string GenerateRefreshToken();
+    ClaimsPrincipal? ValidateToken(string token);
+}
---
+++ src/TokenService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Foundation.AuthenticationHelper;
+public class TokenService: ITokenService
+{
+    private readonly string _secretKey;
+
+    public TokenService(string secretKey)
+    {
+        _secretKey = secretKey;
+    }
+
+    public string GenerateAccessToken(string userId, string role, string ipAddress, string userAgent)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(_secretKey);
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, userId),
+            new Claim(ClaimTypes.Role, role),
+            new Claim("ip", ipAddress),
+            new Claim("browser", userAgent)
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(30), // Token expiry
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    public string GenerateRefreshToken()
+    {
+        var randomNumber = new byte [32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    public ClaimsPrincipal? ValidateToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(_secretKey);
+
+        var validationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        };
+
+        try
+        {
+            var principal = tokenHandler.ValidateToken(token, validationParameters, out _);
+            return principal;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
