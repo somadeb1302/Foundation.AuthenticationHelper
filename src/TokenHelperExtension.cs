3913dc71c1b39f6abb047db352945aa1f93a1fe8
Debaditya De <somadeb1302.de@outlook.com>
2025-01-04 23:36:53 +0530

    extension method added

---
+++ src/TokenHelperExtension.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Sockets;
+
+namespace Foundation.AuthenticationHelper;
+public static class TokenHelperExtension
+{
+
+    public static IServiceCollection AddTokenService(this IServiceCollection services, string key)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+
+        services.AddSingleton<ITokenService>(c =>
+        {
+            var secret = Environment.GetEnvironmentVariable(key);
+            if (string.IsNullOrWhiteSpace(secret))
+                secret = Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.User);
+            
+            if (secret == null) 
+                throw new ArgumentNullException(nameof(services));
+            
+            return new TokenService(secret);
+        });
+
+        return services;
+    }
+}
